b7def6e768a73b4f148013d29509a6588e181d07
Yeonseok <49339278+SongYeonSeok@users.noreply.github.com>
2021-10-03 18:48:51 +0900

    Add files via upload

---
+++ 부스트코스_안드로이드/B. 코틀린 프로그래밍 시작/HelloKt.kt
@@ -0,0 +1,21 @@
+fun main() {
+    println("Hello Kotlin")
+}
+
+
+/* fun main(args: Array<String>) {
+    println(args[0])    // 외부의 첫번째 인자
+    println(args[1])    // 외부의 두번째 인자
+    println(args[2])    // 외부의 세번째 인자
+} */
+
+
+/* import com.sun.istack.internal.NotNull
+
+public final class HelloKt {
+    public static final void main(@NotNull String[] ars ) {
+        Intrinsics.checkParameterIsNotNull(args, "args");
+        String var1 = "Hello Kotlin";
+        System.out.println(var1);
+    }
+} */
